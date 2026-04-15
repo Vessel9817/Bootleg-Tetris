Vessel9817/Bootleg-Tetris

commit 8748e2bf2a8758adc473c18c5fec586b85e79df4

Declared types
Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

src/bootleg_tetris/block.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+from typing import TYPE_CHECKING
+
+# Preventing circular import at runtime
+if TYPE_CHECKING:
+    from .grid import Grid
+
 class Block:
-    ''' A collection of cells on a grid in the form of various tetrominoes in which the player can manipulate and control
+    '''
+    A collection of cells on a grid in the form of various tetrominoes in which the player can manipulate and control
 
     Static Attributes:
         - ROWS : The number of rows in a grid
@@ -96,80 +103,78 @@ class Block:
     __STD_OFFSETS = {
         0 : {
             0 : ((0, 0),),
-            1 : ((0, 0), (-1, 0), (-1, +1), (0, -2), (-1, -2)),
-            2 : ((0, 0), (+1, 0), (+1, +1), (0, -2), (+1, -2)),
-            3 : ((0, 0), (0, +1), (+1, +1), (-1, 1), (+1, 0), (-1, 0))
+            1 : ((0, 0), (-1, 0), (-1, 1), (0, -2), (-1, -2)),
+            2 : ((0, 0), (1, 0), (1, 1), (0, -2), (1, -2)),
+            3 : ((0, 0), (0, 1), (1, 1), (-1, 1), (1, 0), (-1, 0))
         },
         1 : {
-            0 : ((0, 0), (+1, 0), (+1,-1), (0, +2), (1, +2)),
+            0 : ((0, 0), (1, 0), (1,-1), (0, +2), (1, +2)),
             1 : ((0, 0),),
-            2 : ((0, 0), (-1, 0), (-1, 2), (-1, 1), (0, +2), (0, +1)),
-            3 : ((0, 0), (+1, 0), (1, -1), (0, +2), (1, +2))
+            2 : ((0, 0), (-1, 0), (-1, 2), (-1, 1), (0, +2), (0, 1)),
+            3 : ((0, 0), (1, 0), (1, -1), (0, +2), (1, +2))
         },
         2 : {
             0 : ((0, 0), (-1, 0), (-1, -1), (0, +2), (-1, +2)),
-            1 : ((0, 0), (+1, 0), (+1, +2), (1, +1), (0, +2), (0, +1)),
+            1 : ((0, 0), (1, 0), (1, +2), (1, 1), (0, +2), (0, 1)),
             2 : ((0, 0),),
             3 : ((0, 0), (-1, 0), (-1, -1), (0, +2), (-1, +2))
         },
         3 : {
-            0 : ((0, 0), (0, -1), (-1, -1), (1, -1), (-1, 0), (+1, 0)),
-            1 : ((0, 0), (-1, 0), (-1, +1), (0, -2), (-1, -2)),
-            2 : ((0, 0), (+1, 0), (+1, +1), (0, -2), (+1, -2)),
+            0 : ((0, 0), (0, -1), (-1, -1), (1, -1), (-1, 0), (1, 0)),
+            1 : ((0, 0), (-1, 0), (-1, 1), (0, -2), (-1, -2)),
+            2 : ((0, 0), (1, 0), (1, 1), (0, -2), (1, -2)),
             3 : ((0, 0),)
         }
     }
     __I_OFFSETS = {
         0 : {
             0 : ((0, 0),),
-            1 : ((0, 0), (-2, 0), (+1, 0), (-2, -1), (+1, +2)),
+            1 : ((0, 0), (-2, 0), (1, 0), (-2, -1), (1, +2)),
             2 : ((0, 0), (-1, 0), (+2, 0), (-1, +2), (+2, -1)),
-            3 : ((0, 0), (0, +1), (+1, 1), (-1, +1), (+1, 0), (-1, 0))
+            3 : ((0, 0), (0, 1), (1, 1), (-1, 1), (1, 0), (-1, 0))
         },
         1 : {
-            0 : ((0, 0), (+2, 0), (-1, 0), (+2, +1), (-1, -2)),
+            0 : ((0, 0), (+2, 0), (-1, 0), (+2, 1), (-1, -2)),
             1 : ((0, 0),),
-            2 : ((0, 0), (-1, 0), (-1, 2), (-1, +1), (0, +2), (0, +1)),
+            2 : ((0, 0), (-1, 0), (-1, 2), (-1, 1), (0, +2), (0, 1)),
             3 : ((0, 0), (-1, 0), (+2, 0), (-1, +2), (+2, -1))
         },
         2 : {
-            0 : ((0, 0), (+1, 0), (-2, 0), (+1, -2), (-2, +1)),
-            1 : ((0, 0), (+1, 0), (+1, 2), (+1, +1), (0, +2), (0, +1)),
+            0 : ((0, 0), (1, 0), (-2, 0), (1, -2), (-2, 1)),
+            1 : ((0, 0), (1, 0), (1, 2), (1, 1), (0, +2), (0, 1)),
             2 : ((0, 0),),
-            3 : ((0, 0), (-2, 0), (+1, 0), (-2, -1), (+1, +2))
+            3 : ((0, 0), (-2, 0), (1, 0), (-2, -1), (1, +2))
         },
         3 : {
-            0 : ((0, 0), (0, -1), (-1, -1), (1, -1), (-1, 0), (+1, 0)),
+            0 : ((0, 0), (0, -1), (-1, -1), (1, -1), (-1, 0), (1, 0)),
             1 : ((0, 0), (-1, 0), (+2, 0), (-1, +2), (+2, -1)),
-            2 : ((0, 0), (+2, 0), (-1, 0), (+2, +1), (-1, -2)),
+            2 : ((0, 0), (+2, 0), (-1, 0), (+2, 1), (-1, -2)),
             3 : ((0, 0),)
         }
     }
 
-    def __init__(self, block_type, grid, rot_state=0):
+    def __init__(self, block_type: str, grid: 'Grid', rot_state: int=0) -> None:
         '''Constructs a Block object whose grid is *grid*, block type is *block_type*, rotation state is *rot_state*, is offset by 3 columns and whose colour is determined by its block type'''
         self.__grid = grid
 
         self.resetBlock(block_type, rot_state=rot_state)
 
-    def moveDown(self):
+    def moveDown(self) -> bool:
         '''Moves the block down by 1 row from player command. If the block cannot move, it instantly locks the block in place and returns False. If it can move, it restarts the lock countdown timer and turns it off and returns True'''
         self.eraseBlock()
 
         # Checking if block can move down
         if not self.__move(row=-1):
             self.__grid.instantLock()
-
             return False
 
         # Resetting auto-lock timers
         self.__grid.drop_counter = 0
         self.__grid.timer_running = False
         self.__grid.timer = 0
-
         return True
 
-    def autoMoveDown(self):
+    def autoMoveDown(self) -> bool:
         '''Moves the block down by 1 row. If the block cannot move, it starts the countdown timer and checks if the block can be locked onto the grid and returns false. If it can move, it restarts the lock countdown timer and turns it off and reurns true. This method differs from the moveDown method, as it is used by the driving Tetris class to automatically soft-drop the block'''
         self.eraseBlock()
 
@@ -177,47 +182,42 @@ class Block:
         if not self.__move(row=-1):
             self.__grid.timer_running = True
             self.__grid.lock()
-
             return False
 
         # Resetting auto-lock timers
         self.__grid.drop_counter = 0
         self.__grid.timer_running = False
         self.__grid.timer = 0
-
         return True
 
-    def moveLeft(self):
+    def moveLeft(self) -> bool:
         '''Moves the block to the left by 1 column using the move method. Will not do anything if the block cannot move'''
         self.eraseBlock()
-
         return self.__move(col=-1)
 
-    def moveRight(self):
+    def moveRight(self) -> bool:
         '''Moves the block to the right by 1 column using the move method. Will not do anything if the block cannot move'''
         self.eraseBlock()
-
         return self.__move(col=1)
 
-    def hardDrop(self):
+    def hardDrop(self) -> None:
         '''Continuously moves the block down with the moveDown method until the block cannot move down anymore and locks the block in place'''
         while self.moveDown():
-            pass
+            pass # Logic is already handled
 
-    def rotCW(self):
+    def rotCW(self) -> bool:
         '''Rotates the block clockwise by 90 degrees if the block can rotate using the rotate method'''
-        self.__rotate(1)
+        return self.__rotate(1)
 
-    def rotCCW(self):
+    def rotCCW(self) -> bool:
         '''Rotates the block counter clockwise by 90 degrees if the block can rotate using the rotate method'''
-        self.__rotate(3)
+        return self.__rotate(3)
 
-    def rotFull(self):
+    def rotFull(self) -> bool:
         '''Rotates the block by 180 degrees if the block can rotate using the rotate method'''
+        return self.__rotate(2)
 
-        self.__rotate(2)
-
-    def __rotate(self, rot_state_change):
+    def __rotate(self, rot_state_change: int):
         '''Rotates the block in terms of the rotational state change *rot_state_change*, if possible. Also returns a boolean value based on whether rotation is possible or not.
 
         Parameters:
@@ -251,18 +251,18 @@ class Block:
 
         return False
 
-    def __move(self, row=0, col=0):
+    def __move(self, row: int=0, col: int=0) -> bool:
         ''' If the block is not colliding with another block or the grid's edges, it decreases the row offset by *row* and column offset by *col* to move the block and returns True. If the block is colliding with another or the edges, it returns False'''
         if not self.collisionDetect(r_off=row, c_off=col):
             return False
 
-        # Offsetting block position (subtraction is performed to force origin cell to bottom-left hand corner)
+        # Offsetting block position
+        # (subtraction is performed to force origin cell to bottom-left hand corner)
         self.__row_offset -= row
         self.__col_offset += col
-
         return True
 
-    def collisionDetect(self, r_off=0, c_off=0):
+    def collisionDetect(self, r_off: int=0, c_off: int=0) -> bool:
         '''Temporarily offsets the block by the *r_off* and *c_off* to get the coordinates at that position and checks if the coordinates of that possition are within the grid and if they overlap with existing blocks. If they overlap or are not in the grid, it returns False, otherwise it returns True'''
         # Temporarily offsetting block
         self.__row_offset -= r_off
@@ -277,35 +277,34 @@ class Block:
 
         # Performing collision detection with new cell coordinates
         for coords in coordinates:
-            # Checking if block position is in grid boundaries
-            if not (0 <= coords[0] < Block.ROWS and 0 <= coords[1] < Block.COLS):
-                return False
-
-            # Searching for non-empty spaces
-            elif self.__grid.getCell(coords[0], coords[1])[1] != Block.BLACK:
+            in_bounds =  (0 <= coords[0] < Block.ROWS and 0 <= coords[1] < Block.COLS)
+            non_empty = self.__grid.getCell(coords[0], coords[1])[1] != Block.BLACK
+            if not in_bounds or non_empty:
                 return False
 
         return True
 
-    def getBlockType(self):
+    def getBlockType(self) -> str:
         '''Returns the block type
-        
+
         Returns:
             string : This block's block type
         '''
         return self.__block_type
 
-    def eraseBlock(self):
+    def eraseBlock(self) -> None:
         '''Removes the block from the grid by changing all the cells back to black'''
-
         for coords in self.getCoords():
             self.__grid.setCell(coords[0], coords[1], Block.BLACK)
 
-    def getCoords(self):
+    def getCoords(self) -> list[list[int]]:
         '''Returns the current coordinates of the block'''
-        return [[base_coords[0] + self.__row_offset, base_coords[1] + self.__col_offset] for base_coords in Block.__SHAPES[self.__block_type][self.__rot_state]]
+        return [
+            [base_coords[0] + self.__row_offset, base_coords[1] + self.__col_offset]
+            for base_coords in Block.__SHAPES[self.__block_type][self.__rot_state]
+        ]
 
-    def resetBlock(self, block_type, rot_state=0):
+    def resetBlock(self, block_type: str, rot_state: int=0) -> None:
         '''Resets the block by changing block type to *block_type*, rotation state to *rot_state* and other attributes to their base values'''
         self.__block_type = block_type.lower()
         self.__rot_state = rot_state
@@ -313,10 +312,5 @@ class Block:
         self.__col_offset = 3
         self.colour = Block.COLOURS[self.__block_type]
 
-    def __str__(self):
-        '''str override '''
-        return f'Block type: {self.block_type}, Block color: {self.__block_color}, Coordinates: {self.getCoords()}, Row offset: {self.__row_offset}, Column offset: {self.__col_offset}, Rotational state: {self.__rot_state}'
-
     def __repr__(self):
-        '''repr override '''
-        return f"Block('{self.block_type}, {self.__grid}, rot_state={self.rot_state}')"
+        return f"Block('{self.__block_type}, {repr(self.__grid)}, rot_state={self.__rot_state}')"

src/bootleg_tetris/game.py
@@ -7,14 +7,10 @@ from .block import Block
 from .grid import Grid
 
 
-def startGame(display):
+def startGame(display: pygame.surface.Surface) -> None:
     '''Is responsible for: parsing key inputs and redirecting them to controls within the game; for drawing and refreshing the display and grid; for checking whether a player has lost or not and prompting the respective message for such an event and; for the countdown timers of auto dropping and automatically locking the blocks to their respective grids'''
-
-    frame_counter = 0
-
     grid_1 = Grid(100, 100, 400, display)
     grid_2 = Grid(500, 100, 400, display)
-
     keyPressedActions = {
         # Player 1 controls
         pygame.K_w     : grid_1.block.rotCW,
@@ -36,31 +32,26 @@ def startGame(display):
         pygame.K_k     : grid_2.block.hardDrop,
         pygame.K_l     : grid_2.swapHold
     }
-
-    # Tracking in-game time
     clock = pygame.time.Clock()
 
-    # Printing current event until display is exited
+    # Event loop
     while True:
-        frame_counter += 1
-
+        # Displaying game over
         if grid_1.lose:
             grid_2.win = True
             font = pygame.font.SysFont(None, 30)
             new_game_text = font.render("Press Space To Restart", False, Block.WHITE)
-
             display.blit(new_game_text, (display.get_width() / 2.75, 50))
         elif grid_2.lose:
             grid_1.win = True
             font = pygame.font.SysFont(None, 30)
             new_game_text = font.render("Press Space To Restart", False, Block.WHITE)
-
             display.blit(new_game_text, (display.get_width() / 2.75, 50))
 
+        # Handling game events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 raise SystemExit
-
             elif not (grid_1.win or grid_2.win):
                 # Performing key press incurred operations
                 if event.type == pygame.KEYDOWN:
@@ -69,13 +60,12 @@ def startGame(display):
                     else:
                         # Unknown key pressed
                         pass
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    pygame.draw.rect(display, Block.BLACK, pygame.Rect(display.get_width()/2.75, 50, 600, 50))
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                pygame.draw.rect(display, Block.BLACK, pygame.Rect(display.get_width()/2.75, 50, 600, 50))
+                grid_1.resetGrid()
+                grid_2.resetGrid()
 
-                    grid_1.resetGrid()
-                    grid_2.resetGrid()
-
+        # Dropping blocks
         if not (grid_1.win or grid_2.win):
             for g in Grid.GRIDS:
                 g.drop_counter += 1

src/bootleg_tetris/grid.py
@@ -8,7 +8,8 @@ from .block import Block
 
 
 class Grid:
-    """ A coloured grid of square cells in which player interactions with the game are possible.
+    """
+    A coloured grid of square cells in which player interactions with the game are possible.
 
     Static attributes:
         - COLS : The number of columns in the grid
@@ -47,20 +48,20 @@ class Grid:
 
     COLS = 10
     ROWS = 20
-    GRIDS = []
+    GRIDS: list['Grid'] = []
     BLOCKS = ['i', 'j', 'l', 's', 'z', 't', 'o']
-    NEXT_BLOCKS = []
+    NEXT_BLOCKS: list[str] = []
     LEVEL = 0
     SPEED = 35
     __SCORE = {
-        0 : 0, # 0 score, just in case
+        0 : 0, # Just in case
         1 : 40,
         2 : 100,
         3 : 300,
         4 : 1200
     }
 
-    def __init__(self, x:int, y:int, height:int, surface):
+    def __init__(self, x: int, y: int, height: int, surface: pygame.surface.Surface) -> None:
         '''The constructor/initialization method of the grid and its attributes
 
         Parameters:
@@ -84,9 +85,10 @@ class Grid:
         self.hold = Block('?', self)
         self.__block_index = 0
 
+        # Full initialization
         self.resetGrid()
 
-    def resetGrid(self):
+    def resetGrid(self) -> None:
         '''Resets the Grid attributes, excluding unchanged attributes, such as __surface'''
         # Temporarily reinitializing block 
         self.block.resetBlock('?')
@@ -110,9 +112,9 @@ class Grid:
         self.score = 0
 
         # Resetting grid cell colours
-        self.__grid_colours = [
-            [Block.BLACK for j in range(Grid.COLS)]
-            for i in range(Grid.ROWS)
+        self.__grid_colours: list[list[tuple[int, int, int]]] = [
+            [Block.BLACK for _ in range(Grid.COLS)]
+            for _ in range(Grid.ROWS)
         ]
 
         # Resetting grid cells
@@ -124,9 +126,9 @@ class Grid:
                     self.__cellLength,
                     self.__cellLength
                 )
-                for j in range(10)
+                for j in range(Grid.COLS)
             ]
-            for i in range(20)
+            for i in range(Grid.ROWS)
         ]
 
         Grid.LEVEL = 0
@@ -144,21 +146,21 @@ class Grid:
 
         self.__getNextBlock()
 
-    def drawHold(self):
+    def drawHold(self) -> None:
         '''Draws text displaying the player's currently held block'''
         pygame.draw.rect(self.__surface, Block.BLACK, pygame.Rect(self.__x - 100, self.__y, 100, 100))
         font = pygame.font.SysFont(None, 20)
         hold_text = font.render(f'Holding {self.hold.getBlockType()}-block' if self.hold.getBlockType() != '?' else f'Holding nothing', False, Block.WHITE)
         self.__surface.blit(hold_text, (self.__x - 100, self.__y))
 
-    def drawLevel(self):
+    def drawLevel(self) -> None:
         '''Draws text displaying the current level'''
         pygame.draw.rect(self.__surface, Block.BLACK, pygame.Rect(self.__x - 100, self.__y + self.__height // 1.5, 100, 30))
         font = pygame.font.SysFont(None, 20)
         level_text = font.render(f'level {Grid.LEVEL + 1}', False, Block.WHITE)
         self.__surface.blit(level_text, (self.__x - 100, self.__y + self.__height // 1.5))
 
-    def drawLinesCleared(self):
+    def drawLinesCleared(self) -> None:
         '''Draws text displaying the player's current quantity of cleared lines'''
         pygame.draw.rect(self.__surface, Block.BLACK, pygame.Rect(self.__x - 100, self.__y + self.__height - 40, 100, 30))
         font = pygame.font.SysFont(None, 20)
@@ -167,7 +169,7 @@ class Grid:
         self.__surface.blit(lines_text, (self.__x - 100, self.__y + self.__height - 40))
         self.__surface.blit(lines_value, (self.__x - 100, self.__y + self.__height - 20))
 
-    def drawScore(self):
+    def drawScore(self) -> None:
         '''Draws text displaying the player's current score'''
         pygame.draw.rect(self.__surface, Block.BLACK, pygame.Rect(self.__x - 100, self.__y + self.__height // 1.25, 100, 30))
         font = pygame.font.SysFont(None, 20)
@@ -176,71 +178,67 @@ class Grid:
         self.__surface.blit(score_text, (self.__x - 100, self.__y + self.__height // 1.25))
         self.__surface.blit(score_value, (self.__x - 100, self.__y + self.__height // 1.25 + 20))
 
-    def swapHold(self):
+    def swapHold(self) -> None:
         '''Swaps the current block with the currently held block; a process called holding. Can only occur once before this player locks a block'''
         if not self.__is_held:
             self.__is_held = True
-
             self.block.eraseBlock()
 
             if self.hold.getBlockType() == '?':
                 self.flag = False
-
                 self.hold.resetBlock(self.block.getBlockType())
-
                 self.__getNextBlock()
             else:
                 temp = self.hold.getBlockType()
-
                 self.hold.resetBlock(self.block.getBlockType())
                 self.block.resetBlock(temp)
 
             self.drawHold()
 
-    def getX(self):
+    def getX(self) -> int:
         '''Returns the grid's x cooridnate '''
         return self.__x
 
-    def setX(self, x):
+    def setX(self, x) -> None:
         '''Sets the grid's x coordinate to *x* if x is within the bounds of the surface'''
         if 0 <= x <= self.__surface.get_width() - self.__width:
             self.__x = x
 
-    def getY(self):
+    def getY(self) -> int:
         '''Returns the grid's y coordinate'''
         return self.__y
 
-    def setY(self, y):
+    def setY(self, y) -> None:
         '''Sets the grid's y coordiante to *y* if y is within the bounds of the surface'''
         if 0 <= y <= self.__surface.get_height() - self.__height:
             self.__y = y
 
-    def getWidth(self):
+    def getWidth(self) -> int:
         '''Returns the grid's width in pixels'''
         return self.__width
 
-    def setWidth(self, width):
+    def setWidth(self, width) -> None:
         '''Sets the grid's width to *width* if it is within the bounds of the surface'''
         if 0 <= width <= self.__surface.get_width():
             self.__width = width
             self.__height = width * 2
             self.__cellLength = width // Grid.COLS
 
-    def getHeight(self):
+    def getHeight(self) -> int:
         '''Returnes the grid's height in pixels'''
         return self.__height
 
-    def setHeight(self, height):
+    def setHeight(self, height) -> None:
         '''Sets the grid's height to *height* if it is within the bounds of the surface'''
         if 0 <= height <= self.__surface.get_height():
             self.__height = height
             self.__width = height // 2
-            self.__cellLength = height // Grid.ROWs
+            self.__cellLength = height // Grid.ROWS
 
-    def drawGrid(self):
+    def drawGrid(self) -> None:
         '''If the the player has not won or lost, it draws the matrice of rectangles called __grid and draws the grid lines. If the player has lost, draws a big red rectangle with a label on it saying "You Lose". If the player has won, draws a big green rectangle with a label on it saying "You Win". '''
-        # Checking if the game is still in progress
         if not self.lose and not self.win:
+            # Checking if the game is still in progress
             for row in range(len(self.__grid)):
                 for col in range(len(self.__grid[row])):
                     pygame.draw.rect(self.__surface, self.__grid_colours[row][col], self.__grid[row][col])
@@ -250,16 +248,14 @@ class Grid:
 
             for col in range(len(self.__grid[0]) + 1):
                 pygame.draw.rect(self.__surface, Block.WHITE, pygame.Rect(int(self.__x + col * self.__cellLength), self.__y, 1, self.__height))
-
-        # Checking if this player lost
         elif self.lose:
+            # Checking if this player lost
             pygame.draw.rect(self.__surface, Block.RED, pygame.Rect(self.__x, self.__y, self.__width, self.__height))
             font = pygame.font.SysFont(None, 65)
             loss_text = font.render('You Lose', True, Block.WHITE)
             self.__surface.blit(loss_text, (self.__x , self.__y + self.__height // 2))
-
-        # Checking if this player won
         elif self.win:
+            # Checking if this player won
             pygame.draw.rect(self.__surface, Block.GREEN, pygame.Rect(self.__x, self.__y, self.__width, self.__height))
             font = pygame.font.SysFont(None, 65)
             win_text = font.render('You Win', True, Block.WHITE)
@@ -268,8 +264,13 @@ class Grid:
         # Updating window
         pygame.display.update()
 
-    def getCell(self, row, col):
-        '''Returns the grid's indexed cell and colour
+    def getCell(
+        self,
+        row: int,
+        col: int
+    ) -> tuple[pygame.Rect, tuple[int, int, int]]:
+        '''
+        Returns the grid's indexed cell and colour
 
         Parameters:
             - row : The row index of the cell
@@ -280,7 +281,7 @@ class Grid:
         '''
         return self.__grid[row][col], self.__grid_colours[row][col]
 
-    def setCell(self, row, col, colour):
+    def setCell(self, row: int, col: int, colour: tuple[int, int, int]) -> None:
         '''Sets the grid's indexed cell colour to *colour*
 
         Parameters:
@@ -290,12 +291,12 @@ class Grid:
         '''
         self.__grid_colours[row][col] = colour
 
-    def drawBlock(self):
+    def drawBlock(self) -> None:
         '''Draws the current block on the grid'''
         for coords in self.block.getCoords():
             self.setCell(coords[0], coords[1], self.block.colour)
 
-    def lock(self):
+    def lock(self) -> None:
         '''Manages a timer for when the current block should be either moveable or unmoveable when on the ground. Generates a new block if this block is locked (i.e., made unmovable)'''
         if self.timer >= Grid.SPEED * 3 and not self.block.collisionDetect(r_off=-1):
             # Clearing lines and generating block
@@ -310,7 +311,7 @@ class Grid:
             # Starting lock timer
             self.timer_running = True
 
-    def instantLock(self):
+    def instantLock(self) -> None:
         '''Instantly locks the current block to the grid, clears and sends any complete lines. Generates a new block if this block is locked (i.e., made unmovable)'''
         self.__getNextBlock()
         self.__clearLines()
@@ -320,7 +321,7 @@ class Grid:
         self.__is_held = False
         self.timer = 0
 
-    def __receiveLines(self):
+    def __receiveLines(self) -> None:
         '''Detects and collects which rows in the grid will be moved and moves them up by the number of line sent. The empty space will be replaced by gray blocks which represent garbage lines. There will be a randomly selected column in which there will be no garbage lines to represent messiness. If the number of rows moved + the number of lines received exeeds or equals the number of rows on the grid, the player loses.'''
         top_row = None
 
@@ -352,9 +353,9 @@ class Grid:
                     for col in range(Grid.COLS):
                         self.setCell(row, col, Block.GRAY if col != random_col else Block.BLACK)
 
-    def __clearLines(self):
+    def __clearLines(self) -> None:
         '''Finds and stores the rows which can be cleared and clears them while moving the rows above them down by the number of rows cleared. If rows were cleared, the __lines_cleared, score, LEVEL, and SPEED increase and visually update accordingly. Finally, reduces the lines received by the number of lines cleared. If the lines received attribute becomes negative, it sends lines back to the other grid using the GRIDS static list. If the lines received are still positive, it calls the receiveLines method to receive the lines and resets the lines received attribute'''
-        cleared_rows = []
+        cleared_rows: list[int] = []
 
         for i in range(Grid.ROWS):
             clearable = True
@@ -402,7 +403,7 @@ class Grid:
             self.__receiveLines()
             self.lines_received = 0
 
-    def __getNextBlock(self):
+    def __getNextBlock(self) -> None:
         '''Replaces the current block with next block in NEXT_BLOCKS static list. If there are no blocks ahead in queue generates no blocks using the generateBlock method. Increases the block index attribute by 1.'''
         if self.flag:
             self.drawBlock()
@@ -422,7 +423,8 @@ class Grid:
 
         self.__block_index += 1
 
-    def __generateBlock():
+    @staticmethod
+    def __generateBlock() -> None:
         '''Based on certain conditions involving previously generated blocks, generates a new block and appends it to the static list BLOCKS'''
         blocks = Grid.BLOCKS.copy()
 
@@ -441,10 +443,6 @@ class Grid:
         # Appending generated block to queue
         Grid.NEXT_BLOCKS.append(blocks[randint(0, len(blocks) - 1)])
 
-    def __str__(self):
-        '''str override'''
-        return f'Grid: ({self.__grid}), Grid colours: ({self.__gridColours}))'
-
-    def __repr__(self):
-        '''repr overide'''
-        return f'Grid({self.__x}, {self.__y}, {self.__height})'
+    def __repr__(self) -> str:
+        '''repr override'''
+        return f'Grid({self.__x}, {self.__y}, {self.__height}, {repr(self.__surface)})'

commit 43bdb843179fd70aea7f90f21c404515e10901ba

Fixed collision detection bug
Bug was introduced in 8748e2bf2a8758adc473c18c5fec586b85e79df4

Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

src/bootleg_tetris/block.py
@@ -277,9 +277,12 @@ class Block:
 
         # Performing collision detection with new cell coordinates
         for coords in coordinates:
-            in_bounds =  (0 <= coords[0] < Block.ROWS and 0 <= coords[1] < Block.COLS)
+            in_bounds = (0 <= coords[0] < Block.ROWS and 0 <= coords[1] < Block.COLS)
+            if not in_bounds:
+                return False
+
             non_empty = self.__grid.get_cell(coords[0], coords[1])[1] != Block.BLACK
-            if not in_bounds or non_empty:
+            if non_empty:
                 return False
 
         return True